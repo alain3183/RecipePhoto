671da731854653d6448b119394a0d55010a41040
Alain Malenfant <alainmalenfant@yahoo.com>
2013-11-23 18:49:13 -0500

    RecipePhotoVC.m: numberOfItemsInSectin and cellForItemAtIndexPath methods

--- RecipePhoto/RecipeCollectionViewController.m
+++ RecipePhoto/RecipeCollectionViewController.m
@@ -17,6 +17,20 @@
 
 @implementation RecipeCollectionViewController
 
+- (NSInteger) collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section{
+    return recipesPhotos.count;
+}
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath{
+    static NSString *identifier = @"Cell";
+    
+    UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:identifier forIndexPath:indexPath];
+    
+    UIImageView *recipeImageView = (UIImageView *)[cell viewWithTag:100];
+    recipeImageView.image = [UIImage imageNamed:[recipesPhotos objectAtIndex:indexPath.row]];
+    
+    return cell;
+}
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
@@ -30,8 +44,7 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
-    recipesPhotos = [NSArray arrayWithObjects:@"angry_bird_cake.jpg", @"creme_brelee.jpg", @"full_breakfast.jpg",@"green_tea.jpg", @"ham_and_cheese_panini.jpg", @"ham_and_egg_sandwich.jpg", @"hamburger.jpg", @"instant_noodle_with_egg.jpg", @"japanese_noodle_with_pork.jpg", @"mushroom_risotto.jpg", @"noodle_with_bbq_pork.jpg", @"starbucks_coffee.jpg", @"thai_shrimp_cake.jpg", @"vegetable_curry.jpg", @"white_chocolate_donut.jpg", nil
-                     ];
+    recipesPhotos = [NSArray arrayWithObjects:@"angry_bird_cake.jpg", @"creme_brelee.jpg", @"full_breakfast.jpg",@"green_tea.jpg", @"ham_and_cheese_panini.jpg", @"ham_and_egg_sandwich.jpg", @"hamburger.jpg", @"instant_noodle_with_egg.jpg", @"japanese_noodle_with_pork.jpg", @"mushroom_risotto.jpg", @"noodle_with_bbq_pork.jpg", @"starbucks_coffee.jpg", @"thai_shrimp_cake.jpg", @"vegetable_curry.jpg", @"white_chocolate_donut.jpg", nil];
 }
 
 - (void)didReceiveMemoryWarning
